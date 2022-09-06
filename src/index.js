16d71255a3b81aa3f1fdcf1be3a6b1a27de873ea
Akash Nai <akashnai632@gmail.com>
2022-09-06 21:37:03 +0530

    sending location in chat app

--- src/index.js
+++ src/index.js
@@ -18,9 +18,20 @@ app.use(express.static(publicDirectoryPath))
 
 io.on('connection', (socket) => {
     console.log('New web socket connection')
+
+    socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has joined!')
     
     socket.on('sendMessage', (message) => {
         io.emit('message', message)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
+    })
+
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
     })
 })
 
